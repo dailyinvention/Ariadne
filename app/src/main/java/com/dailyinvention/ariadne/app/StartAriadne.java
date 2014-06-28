e5725114112c25819e6d9a3571856de6baf6aeb5
Stefan Holodnick <stefan@dailyinvention.com>
2014-06-28 16:03:56 -0400

    Added getLastLocation class for doing geolocation and modified the view to display current latitude and longitude.

--- app/src/main/java/com/dailyinvention/ariadne/app/StartAriadne.java
+++ app/src/main/java/com/dailyinvention/ariadne/app/StartAriadne.java
@@ -2,36 +2,59 @@ package com.dailyinvention.ariadne.app;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.glass.app.Card;
 import com.google.android.glass.media.Sounds;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class StartAriadne extends Activity {
-    private Card _card;
-    private View _cardView;
-    private TextView _statusTextView;
 
-    private TextToSpeech _speech;
+    private Context context = this;
 
-    private Context _context = this;
+    public static LocationManager manager;
+    public static AudioManager mAudioManager;
 
-    AudioManager mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_ariadne);
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        //setContentView(R.layout.activity_start_ariadne);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String latitude = String.valueOf(getLastLocation(this).getLatitude());
+        String longitude = String.valueOf(getLastLocation(this).getLongitude());
+        Card card1 = new Card(context);
+        card1.setText("Latitude: " + latitude + "\r\n" + "Longitude: " + longitude);
+        View card1View = card1.getView();
+        setContentView(card1View);
+        Log.i("Location: ", latitude);
     }
 
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
             openOptionsMenu();
@@ -43,6 +66,32 @@ public class StartAriadne extends Activity {
             this.finish();
         }
         return false;
+    }
+
+    public static Location getLastLocation(Context context) {
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.NO_REQUIREMENT);
+        List<String> providers = manager.getProviders(criteria, true);
+        List<Location> locations = new ArrayList<Location>();
+        for (String provider : providers) {
+            Location location = manager.getLastKnownLocation(provider);
+            if (location != null && location.getAccuracy() != 0.0) {
+                locations.add(location);
+            }
+        }
+        Collections.sort(locations, new Comparator<Location>() {
+            @Override
+            public int compare(Location location, Location location2) {
+                return (int) (location.getAccuracy() - location2.getAccuracy());
+            }
+        });
+        if (locations.size() > 0) {
+            return locations.get(0);
+        }
+        else {
+            return null;
+        }
     }
 
 }
